64b37891d5667bcfcbbe7f8b93f235679e516823
Jordan Lesaca <Jordanlesaca@gmail.com>
2021-12-19 18:04:22 -0800

    done

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,12 @@
+#author.rb
 class Author < ApplicationRecord
+    validates :name, presence: true
+    validates :name, uniqueness: true
+    validates :phone_number, length: {is: 10}
 end
+
+#1 => can be created with valid data
+#1 => Basic Validations
+#1 => Is expected to validate that :name cannot be empty/falsy
+#1 => Is expected to validate that :name is case-senstivity unique
+#1 => Is expected to validate that the length of :phone_number is 10
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,32 @@
+#post.rb
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    validate :is_clickbait
+
+    CLICKBAIT_PATTERNS = [
+        /Won't Believe/i, 
+        /Secret/i, 
+        /Top \d/i, 
+        /Guess/i
+    ]
+
+    def is_clickbait
+        if CLICKBAIT_PATTERNS.none? { |p| p.match title}
+            errors.add(:title, "must be clickbait")
+        end
+    end
+
 end
+
+#2 => Can be created with valid data
+#2 => Basic Validations
+#2 => Is expected to validate that :title cannot be empty/falsy
+#2 => Is expected to validate that the length of :content is at least 250
+#2 => Is expected to validate that :category is either <"Fiction"> or <"Non-Fiction">
+
+#3 => Custom Validations: title is clickbait
+#3 => Is expected to allow :title to be <"You Won't Believe These True Facts">
+#3 => Is expected not to allow :title to be <"True Facts">
